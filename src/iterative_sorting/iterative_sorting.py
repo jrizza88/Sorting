ce60f196c4093b23d7da79b2156c4203b496ed4b
Jamar Torres <Jetorres88@gmail.com>
2019-12-04 23:27:10 -0500

    completed MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,28 +5,19 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # print(f'smallest {smallest_index}')
         for j in range(cur_index, len(arr)):
-            ## write if statement here
-            if arr[j] < arr[smallest_index]:
+            if arr[smallest_index] > arr[j]:
                 smallest_index = j
 
-        temp_hold = arr[cur_index]
+        temp_index = arr[cur_index]
+        # print(f'temp index{arr[cur_index]}')
         arr[cur_index] = arr[smallest_index]
-        arr[smallest_index] = temp_hold
-        # print(f'smallest {smallest_index}')
+        # print(f'second temp index{arr[smallest_index]}')
+        arr[smallest_index] = temp_index
+
     return arr
-                
-
-#    For all indices EXCEPT the last index:
-
-# a. Loop through elements on right-hand-side of 
-# current index and find the smallest element
-
-# b. Swap the element at current index with the 
-# smallest element found in above loop
-
+            
                 
 
 
@@ -35,15 +26,35 @@ def selection_sort( arr ):
 
 
 
-
-    return arr
-
-
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
+def bubble_sort2( arr ):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+        #for each element in list look at element to its right
+        #if out of order to each other, (val on left > val on right)
+        # swap them! 
+        #  arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+        # once highest value element is at the end, it remains there.
     return arr
 
+def bubble_sort( arr ):
+    swap = True
+    while swap:
+        swap = False
+        for i in range(0, len(arr) - 1):
+            largest_index = i
+            if arr[largest_index] > arr[largest_index + 1]:
+                #    arr[largest_index] = arr[largest_index + 1]
+                temp = arr[largest_index]
+                arr[largest_index] = arr[largest_index + 1]
+                arr[largest_index + 1] = temp
+                swap = True
+ 
+    return arr
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
